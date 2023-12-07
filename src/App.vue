ea948ae5af4c5ba1feb4dac1b8568a147a9dcfcc
Paola Cernada <pncp2021@gmail.com>
2023-12-07 17:39:49 -0500

    created note app using Vue, add and delete working, edit does not.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,153 @@
-<script setup>
-</script>
-
 <template>
-  <div>
-    <h1>Vue Notes</h1>
-    <!-- your code here -->
+
+  <div id="noteCardContainer" class="noteCard">
+    <div class="windowTitleBar">
+      <span class="windowTitle">Note App</span>
+      <div class="windowControls"></div>
+    </div>
+    <div class="noteContent">
+      <form @submit.prevent="createNote">
+        <input
+          type="text"
+          v-model="newNote.title"
+          placeholder="Title"
+          required
+        />
+        <textarea
+          v-model="newNote.body"
+          placeholder="Note body"
+          required
+        ></textarea>
+        <button type="submit">Create Note</button>
+      </form>
+
+      <h1 class="noteHeader">Vue Notes</h1>
+
+      <div id="container">
+        <NoteCard
+          v-for="note in notes"
+          :key="note.id"
+          :note="note"
+          @edit-note="displayEditForm"
+          @delete-note="deleteNote"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+import NoteCard from "./components/NoteCard.vue";
+import { ref } from "vue";
+
+export default {
+  components: {
+    NoteCard,
+  },
+  setup() {
+    const notes = ref([]);
+    const newNote = ref({ title: "", body: "" });
+    const url = "http://localhost:3000/notes/";
+
+    const fetchNotes = async () => {
+      const response = await fetch(url);
+      notes.value = await response.json();
+    };
+
+    const createNote = async () => {
+      await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          ...newNote.value,
+          updatedAt: new Date().toISOString(),
+        }),
+      });
+      newNote.value = { title: "", body: "" };
+      fetchNotes();
+    };
+
+    const deleteNote = async (noteId) => {
+      await fetch(url + noteId, { method: "DELETE" });
+      fetchNotes();
+    };
+
+    fetchNotes();
+
+    return { notes, newNote, createNote, deleteNote };
+  },
+};
+</script>
+
 <style scoped>
+.noteContent,
+#noteCardContainer {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-gap: 20px;
+}
+
+form {
+  background: #FFFFFF;
+  padding: 20px;
+  border: 2px solid #B0C4DE;
+  border-radius: 8px;
+  box-shadow:
+    inset 0 0 10px #e6e1e8, /* Inner shadow for a pressed effect */
+    0 4px 6px rgba(0, 0, 0, 0.1); /* Drop shadow for depth */
+  margin-bottom: 20px;
+  width: 50%; 
+  margin-left: auto; 
+  margin-right: auto; 
+}
+
+input[type="text"],
+textarea {
+  font-family: "Press Start 2P", monospace;
+  color: #4a6572;
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+  background-color: #e6e1e8; 
+  border: 1px solid #B0C4DE;
+  border-radius: 4px; 
+  box-sizing: border-box;
+}
+
+input[type="text"]:focus,
+textarea:focus {
+  outline: none;
+  box-shadow: 0 0 5px #637a91; /* Subtle focus glow */
+}
+
+textarea {
+  resize: vertical;
+  height: 100px;
+}
+
+button {
+  font-family: "Press Start 2P", monospace;
+  background-color: #FFD54F; /* Muted button color */
+  color: #4a6572;
+  border: none;
+  padding: 5px 10px;
+  margin: 3px;
+  text-transform: uppercase;
+  cursor: pointer;
+  border-radius: 4px; 
+  box-shadow:
+    0 2px #B0C4DE, /* Bottom border for 3D effect */
+    0 4px 6px rgba(0, 0, 0, 0.2); /* Drop shadow for depth */
+}
+
+button:hover {
+  background-color: #FFCA28; /* Slightly darker yellow on hover */
+}
+
+#container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-gap: 20px;
+}
+
 </style>
---
+++ src/components/NoteCard.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="noteCard">
+    <div class="noteHeader">{{ note.title }}</div>
+    <div>{{ note.body }}</div>
+    <div>Last updated: {{ formatDate(note.updatedAt) }}</div>
+    <button @click="editNote">EDIT</button>
+    <button @click="deleteNote">DELETE</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["note"],
+  emits: ["edit-note", "delete-note"],
+  methods: {
+    editNote() {
+      this.$emit("edit-note", this.note.id);
+    },
+    deleteNote() {
+      this.$emit("delete-note", this.note.id);
+    },
+    formatDate(dateString) {
+      const date = new Date(dateString);
+      return (
+        date.toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        }) +
+        " at " +
+        date.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" })
+      );
+    },
+  },
+};
+</script>
+
+<style scoped>
+.noteCard {
+  background: #eceff1; /* Light grey-blue for cards */
+  font-family: "Roboto Mono", monospace;
+  border: 2px solid #dfe3e7; /* Classic Windows border color */
+  padding: 15px;
+  border-radius: 8px; /* Slightly rounded edges for a softer look */
+  box-shadow:
+    inset 0 0 10px #e6e1e8,
+    /* Inner shadow for a pressed effect */ 0 2px 4px rgba(0, 0, 0, 0.1); /* Drop shadow for depth */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+}
+
+.noteHeader {
+  font-family: "Press Start 2P", cursive;
+  font-size: medium;
+}
+
+#noteTitle {
+  font-size: small;
+}
+
+.hidden {
+  display: none;
+}
+
+.noteCard div {
+  margin-bottom: 10px; /* Space between the divs and button */
+}
+
+.noteCard button {
+  align-self: flex-end;
+  margin-top: auto; /* Pushes the button to the bottom */
+}
+
+button,
+.noteCard {
+  transition: transform 0.2s, box-shadow 0.2s;
+  display: inline-block;
+}
+
+.noteCard:hover {
+  transform: translateY(-5px);
+  box-shadow:
+    0 4px 8px rgba(0, 0, 0, 0.2), /* Subtle hover shadow for lift effect */
+    inset 0 0 15px #e6e1e8; /* Inner shadow for a pressed effect */
+}
+
+.noteContent {
+    padding: 10px; /* Padding inside the card for content */
+  }
+
+#date {
+  font-family: 'Roboto Mono', monospace;
+  font-size: small;
+}
+
+</style>
